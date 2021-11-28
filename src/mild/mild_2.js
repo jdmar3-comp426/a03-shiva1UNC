d6913db0382199f9c3c8b01e7b98785cafe109a3
shiva1UNC <shiva1@live.unc.edu>
2021-11-28 01:00:49 -0500

    create function

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -74,7 +74,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let newObj = object;
+   let newObj = create(object);
    removeKey(newObj, key);
    return newObj;
 }
