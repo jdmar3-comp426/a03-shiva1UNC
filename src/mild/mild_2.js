697a244fd52b007fa9ad7fb975f4581b1128a586
shiva1UNC <shiva1@live.unc.edu>
2021-11-30 00:35:54 -0500

    added assign

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -74,7 +74,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let newObj = Object.create(object);
+   let newObj = Object.assign({}, object);
    removeKey(newObj, key);
    return newObj;
 }
