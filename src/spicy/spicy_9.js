cad6ffdfd05c3acb0e3bd002305d8cc702b94e6a
shiva1UNC <shiva1@live.unc.edu>
2021-11-30 15:11:44 -0500

    used spread

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,9 +15,9 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
-//    let return_vals = [];
+    let return_vals = [];
     for (let i = 0; i < n; i++) {
-        fn(params);
+        return_vals.push(fn(...params));
     }
     return fn(params);
 };
