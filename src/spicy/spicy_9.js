7bbeb00d982b227f45685f856b4897b5f1632d03
shiva1UNC <shiva1@live.unc.edu>
2021-11-30 22:36:59 -0500

    evens

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -94,7 +94,12 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    for(let i = 0; i < arr.length; i+=2) {
+        if(!test(arr[i])) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
@@ -118,7 +123,12 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    for(let i = 0; i < arr.length; i+=2) {
+        if(!test(arr[i])) {
+            return true;
+        }
+    }
+    return false;
 };
 
 
